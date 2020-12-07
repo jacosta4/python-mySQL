bf3e5a43a87bced1612e5aa93918eeb847972785
jacosta4 <jacosta441@gmail.com>
2020-12-06 20:03:41 -0500

    añade funciones de busqueda y mostrar ususarios

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import pyFuncs
 
 mydb = mysql.connector.connect(
     host = 'localhost',
@@ -7,15 +8,44 @@ mydb = mysql.connector.connect(
     database = 'my_community'
 )
 
-cursor = mydb.cursor()
+cursor = mydb.cursor(dictionary=True)
 
-def create_user(username, first_name, last_name, age, email, country):
+def create_user(username, first_name, last_name, age, email, country, join_date):
     sql = """
         INSERT INTO users (username, first_name, last_name,
-        age, email, country)
-        VALUES (%s, %s, %s, %s, %s, %s )"""
-    values = (username, first_name, last_name, age, email, country)
+        age, email, country, join_date)
+        VALUES (%s, %s, %s, %s, %s, %s, %s )"""
+    values = (username, first_name, last_name, age, email, country, join_date)
     cursor.execute(sql, values)
     mydb.commit()
 
-    # mydb.close()
+    mydb.close()
+
+def show_all_users():
+    query = """
+            SELECT user_ID, username, join_date
+            FROM users
+            """
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    mydb.commit()
+    mydb.close()
+
+# buscar username en db
+def search_username(username1):
+    query = """
+            SELECT username
+            FROM users
+            WHERE username = %s
+            """
+    value = (username1,)
+    cursor.execute(query,value)
+    data = cursor.fetchall()
+    if len(data) == 0:
+        return f"Username dont exists"
+    else:
+        return f"Username already exists"
+    mydb.commit()
+    mydb.close()
--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import pyinputplus as pyip
 import db
+import pyFuncs
 
-print('-- WELCOME!!--')
-print('\nEnter the following data')
+print('\n-- WELCOME!!--')
+print('\nEnter your Username please')
 
-username = pyip.inputStr(prompt='Username: ')
-first_name = pyip.inputStr(prompt='First Name: ')
-last_name = pyip.inputStr(prompt='Last Name: ')
-age = pyip.inputInt(prompt='Age: ', min=1, max=99)
-email = pyip.inputEmail(prompt='Email: ', )
-country = pyip.inputStr(prompt='Country: ')
+username1 = pyip.inputStr(prompt='Username: ')
+user_vali = db.search_username(username1)
+if user_vali == 'Username dont exists':
+    print(f"Username dont exists, we're going to create your profile!")
+    print('\nEnter the following data')
+    username = pyip.inputStr(prompt='Username: ')
+    first_name = pyip.inputStr(prompt='First Name: ')
+    last_name = pyip.inputStr(prompt='Last Name: ')
+    age = pyip.inputInt(prompt='Age: ', min=1, max=99)
+    email = pyip.inputEmail(prompt='Email: ', )
+    country = pyip.inputStr(prompt='Country: ')
+    join_date = pyFuncs.get_date()
 
-# Guardar en DB los datos ingresados
-db.create_user(username, first_name, last_name, age, email, country)
-print('\nUser created successfully')
+    #Guardar en DB los datos ingresados
+    db.create_user(username, first_name, last_name, age, email, country, join_date)
+    print('\nUser created successfully')
+else:
+    print('User exits')
+
+
+# Mostrar usuarios base de datos
+# db.show_all_users()
